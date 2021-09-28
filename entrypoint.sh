e30d0d9838e94caced0a714edebe3cdae1e9031d
Ammar Hussein <ammar.atef45@gmail.com>
2021-09-28 14:49:53 -0700

    x

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh -l
 
 pull_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
-repo=${{ github.repository }}
-echo "::set-output name=url::$repo"
+echo "::set-output name=url::$GITHUB_REPOSITORY"
